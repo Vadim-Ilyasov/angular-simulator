defcbb953885fcda6f25cfdda3540484dab96602
VadimIlyasov <vzilyas89@gmail.com>
2026-02-02 22:02:19 +0500

    fixed tasks

--- src/app/training.ts
+++ src/app/training.ts
@@ -14,21 +14,24 @@ let uploadStatus: 'loading' | 'succes' | 'error';
 
 let textFormat: 'uppercase' | 'lowercase' | 'capitalize';
 
-function calc(a: number, b: number): number {
+function getCalc(a: number, b: number): number {
   return a + b;
 }
 
-function getTextFormat(text: string, textFormat: 'uppercase' | 'lowercase' | 'capitalize') {
-  if (textFormat === 'uppercase') return text.toUpperCase();
-  if (textFormat === 'lowercase') return text.toLowerCase();
+function getTextFormat(text: string, textFormat: 'uppercase' | 'lowercase' | 'capitalize'): string {
+  if (textFormat === 'uppercase') {
+    return text.toUpperCase();
+  }
+  if (textFormat === 'lowercase') {
+    return text.toLowerCase();
+  }
   if (textFormat === 'capitalize') {
-    if (!text) return text;
-    return text.charAt(0).toUpperCase() + text.slice(1);
+    return text ? text.charAt(0).toUpperCase() + text.slice(1) : text;
   }
   return text;
 }
 
-function getTextWithoutSymbol(text: string, symbol: string) {
+function getTextWithoutSymbol(text: string, symbol: string): string {
   if (text.includes(symbol)) {
     return text.split(symbol).join('');
   } else {
@@ -71,5 +74,5 @@ const users: IUser[] = [
   },
 ];
 
-const matureUsers = users.filter(user => user.age > 30);
+const matureUsers: IUser[] = users.filter((user: IUser) => user.age > 30);
 console.log(matureUsers);
